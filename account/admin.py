d941f459fe16ca49243cdcc3d34093176a3eb2d1
Suriya Prakash <suriyag1999@gmail.com>
2024-10-11 14:55:23 +0000

    minor change in db models

--- account/admin.py
+++ account/admin.py
@@ -2,8 +2,8 @@ from django.contrib import admin
 from .models import UserToken
 
 class UserTokenAdmin(admin.ModelAdmin):
-    list_display = ('user','token','expiration_date', 'used', 'expired')
-    list_filter = ("used", "expired", "expiration_date")
+    list_display = ('user','token','expiration_date', 'expired')
+    list_filter = ("expired", "expiration_date")
     search_fields = ['user']
     
 admin.site.register(UserToken, UserTokenAdmin)
